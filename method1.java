a6f9c635016cb5aae4f0ee3503ad1d06f0b96e45
Sowntharyasowmi <S.Sowntharya@gmail.com>
2022-03-24 13:10:43 +0530

    method1

---
+++ method1.java
@@ -0,0 +1,16 @@
+public class method1 {
+    static void myMethod() {
+        System.out.println("am interested in java");
+    }
+        public static void main(String[]args){
+        myMethod();
+        myMethod();
+        myMethod();
+        myMethod();
+        myMethod();
+        myMethod();
+        myMethod();
+    myMethod();
+
+    }
+}
